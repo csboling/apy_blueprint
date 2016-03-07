82f908991c54f90121e17a13f05da1bf753db4b9
Sam Boling <charles.samuel.boling@gmail.com>
2016-03-06 20:30:17 -0500

    apib skeleton generation working

--- apib.py
+++ apib.py
@@ -1,7 +1,7 @@
 from markdown import Markdown
-import flask_restful.reqparse
-from flask_restplus import marshal_with
-import flask_restplus.fields as model_fields
+from flask_restful import reqparse
+from flask.ext.restplus import marshal_with, Resource
+import flask.ext.restplus.fields as model_fields
 
 from funcy import compose, partial, rpartial, constantly
 
@@ -12,22 +12,49 @@ class Parser:
   @property
   def typename_map(self):
     return {
-      'number':  lambda f: model_fields.Float(),
-      'boolean': lambda f: model_fields.Boolean(),
-      'string':  lambda f: model_fields.String(),
-      'array':   self.array_to_field,
-      'object':  self.object_to_field,
-      'enum':    self.enum_to_field
+      'number': {
+        'marshaler':
+          lambda f: model_fields.Float(),
+        'pytype': float,
+      },
+      'boolean': {
+        'marshaler':
+          lambda f: model_fields.Boolean(),
+        'pytype':
+          bool,
+      },
+      'string': {
+        'marshaler':
+          lambda f: model_fields.String(),
+        'pytype':
+          str,
+      },
+      'array': {
+        'marshaler':
+          self.array_to_field,
+        'pytype':
+          list,
+      },
+      'object': {
+        'marshaler':
+          self.object_to_field,
+        'pytype':
+          dict,
+      },
+      'enum': {
+        'marshaler':
+          self.enum_to_field,
+      },
     }
 
   def lookup_datastruct(self, typename):
     try:
       structure = self.api._data_structures[typename]
     except KeyError:
-      import pdb; pdb.set_trace()
+      print('data structure {} not defined'.format(typename))
+      raise
     else:
-      print('found {}'.format(structure))
-      return self.object_to_field(structure) #map(self.parse_field, field.value)
+      return self.object_to_field(structure)
 
   def array_to_field(self, field):
     inside = field.extract_array_subtype(field.type)
@@ -48,31 +75,46 @@ class Parser:
 
   def get_typename_marshaler(self, typename):
     try:
-      return self.typename_map[typename]
+      return self.typename_map[typename]['marshaler']
     except KeyError:
       return (lambda field: self.lookup_datastruct(typename))
 
   def parse_field(self, field):
     outer_typename = field.type.split('[', 1)[0]
     typename_marshaler = self.get_typename_marshaler(outer_typename)
-    ret = (field.name, typename_marshaler(field))
-    # print(ret)
-    return ret
+    return (field.name, typename_marshaler(field))
+
+  def add_parameters(self, parser, parameters):
+    for parameter in parameters:
+      parser.add_argument(
+        parameter.name,
+        dest=parameter.name,
+        location='form',
+        type=self.typename_map[parameter.type]['pytype'],
+        required=parameter.required,
+        help=parameter.description,
+        default=parameter.value
+      )
+
+
 
 class Schema:
-  def __init__(self, apib):
+  def __init__(self, api, apib):
+    self.api = api
+
     markdown_parser = Markdown(extensions=['plueprint'])
     markdown_parser.set_output_format('apiblueprint')
     with open(apib, 'r') as f:
-      self.api = markdown_parser.convert(f.read())
-    self.parser = Parser(self.api)
+      self.api_spec = markdown_parser.convert(f.read())
+    self.parser = Parser(self.api_spec)
     self.routes = {
-      resource.uri: resource
-      for resource in self.api.resources
+      resource.uri_template.uri: resource
+      for resource in self.api_spec.resources
     }
 
   @staticmethod
   def decorate_method(impl, method):
+    print('decorating {}::{}'.format(impl, method))
     try:
       undecorated_method = getattr(impl, method)
     except AttributeError:
@@ -82,39 +124,37 @@ class Schema:
         )
       )
       raise
-
-    print(impl.fields)
-
     setattr(
       impl,
       method,
-      marshal_with(impl.fields)(undecorated_method)
+      marshal_with(impl.model)(undecorated_method)
     )
 
-  @staticmethod
-  def add_parameters(parser, parameters):
-    for parameter in parameters:
-      import pdb; pdb.set_trace()
-      parser.add_argument(
-        parameter.name,
-        dest=parameter.name
-      )
-
-  def get_fields(self, action):
+  def get_model(self, action):
     fields = {}
     for response in action.responses[200]:
       fields.update(dict(map(self.parser.parse_field, response.attributes)))
     return fields
 
-  def decorate(self, resource):
-    def decorator(impl):
-      impl.parser = flask_restful.reqparse.RequestParser()
+  def decorate(self, resource, uri):
+    def decorator(undecorated):
+      impl = type(
+        undecorated.__name__,
+        (Resource,) + undecorated.__bases__,
+        dict(undecorated.__dict__)
+      )
+
+      impl.parser = reqparse.RequestParser()
+
+      if resource.parameters:
+        self.parser.add_parameters(impl.parser, resource.parameters)
+
       for action in resource._actions.values():
-        if action.parameters:
-          self.add_parameters(impl.parser, action.parameters)
-        impl.fields = self.get_fields(action)
-        print(impl.fields)
+        impl.model = self.get_model(action)
         self.decorate_method(impl, action.request_method.lower())
+
+      print('adding {} to {} at {}'.format(impl, self.api, uri))
+      self.api.add_resource(impl, uri)
       return impl
     return decorator
 
@@ -125,4 +165,4 @@ class Schema:
       print('no resource specified at {}'.format(uri))
       raise
     else:
-      return self.decorate(resource)
+      return self.decorate(resource, uri)
